53328b6f0790214046b886309178d0170c35c395
Mohammad Farukh <65119875+iamFarukh@users.noreply.github.com>
2022-06-10 15:00:35 +0530

    Create findMajorityElement.cpp
    findMajorityElement using Boyer-Moore Majority Voting Algorithm

---
+++ Day 4/findMajorityElement.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h> 
+int findMajorityElement(int arr[], int n) {
+	int count = 0;
+    int candidate = 0;
+    for(int i = 0;i<n;i++){
+        if(count == 0) candidate = arr[i];
+        if(candidate == arr[i]) count++;
+        else count--;
+    }
+    count = 0;
+    for(int i = 0;i<n;i++){
+        if(arr[i] == candidate) count++;
+    }
+    if(count<=n/2) return -1;
+    return candidate;
+}
