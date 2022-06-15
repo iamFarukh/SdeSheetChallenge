5409f9a5907f9e158d5cee0f688de79d6ae37db5
Mohammad Farukh <65119875+iamFarukh@users.noreply.github.com>
2022-06-15 14:08:38 +0530

    Create detectCycle.cpp
    detectCycle

---
+++ Day 10/detectCycle.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h> 
+/****************************************************************
+
+    Following is the class structure of the Node class:
+
+        class Node
+        {
+        public:
+	        int data;
+	        Node *next;
+	        Node(int data)
+	        {
+		        this->data = data;
+		        this->next = NULL;
+	        }
+        };
+
+*****************************************************************/
+
+bool detectCycle(Node *head)
+{
+	//    Write your code here
+    if(head ==NULL){
+        return false;
+    }
+    Node *fast=head;
+    Node *slow=head;
+
+    while(fast->next!=NULL && fast->next->next!=NULL){
+        slow=slow->next;
+        fast=fast->next->next;
+        if(slow==fast){
+            return true;
+        }
+    }
+    return false;
+}
