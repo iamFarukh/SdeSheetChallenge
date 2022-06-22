d5ecdc3471ffc8c927205897056d8a9a31871a04
Mohammad Farukh <65119875+iamFarukh@users.noreply.github.com>
2022-06-22 12:29:29 +0530

    Update findNthRootOfM.cpp

--- Day 17/findNthRootOfM.cpp
+++ Day 17/findNthRootOfM.cpp
@@ -4,3 +4,32 @@ double findNthRootOfM(int n, long long m) {
     double ans=pow(m,t);
     return ans;
 }
+
+
+// Here is one more method to do the same
+// using the binary search method
+double multiply(double mid, int n){
+    double ans = 1.0;
+    while(n--){
+        ans *= mid;
+    }
+    return ans;
+}
+
+double findNthRootOfM(int n, long long m) {
+    // Write your code here.
+     double low = 1;
+    double high = m;
+    double eps = 1e-;
+    
+    while((high-low)>eps){
+        double mid = (low + high)/2;
+        if(multiply(mid,n) < m){
+            low = mid;
+        }
+        else {
+            high = mid;
+        }
+    }
+    return low;
+}
