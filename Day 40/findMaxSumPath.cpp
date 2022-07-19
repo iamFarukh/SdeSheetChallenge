5cf87b09d9a3c42e77ffe34e620373b723dc0085
Mohammad Farukh <65119875+iamFarukh@users.noreply.github.com>
2022-07-19 17:26:33 +0530

    Create findMaxSumPath.cpp
    findMaxSumPath

---
+++ Day 40/findMaxSumPath.cpp
@@ -0,0 +1,51 @@
+/************************************************************
+
+    Following is the Tree node structure
+	
+	template <typename T>
+    class TreeNode 
+    {
+        public : 
+        T val;
+        TreeNode<T> *left;
+        TreeNode<T> *right;
+
+        TreeNode(T val) 
+        {
+            this -> val = val;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+************************************************************/
+
+#define lli long long int
+
+int count;
+lli solve(TreeNode<int> *root, lli& ans)
+{
+    if(root == NULL) return 0;
+    
+    if(root -> left == NULL && root -> right == NULL) count++;
+    lli left = solve(root -> left, ans);
+    lli right = solve(root -> right, ans);
+    
+    lli temp = max(left, right) + root -> val;
+    
+    lli mx = max(temp, left + right + root -> val);
+    ans = max(ans, mx);
+    
+    return temp;
+}
+long long int findMaxSumPath(TreeNode<int> *root)
+{
+    // Write your code here.
+    if(root == NULL) return -1;
+    count = 0;
+    lli ans = 0;
+    solve(root, ans);
+   
+    if(count == 1) return -1;
+    return ans;
+}
