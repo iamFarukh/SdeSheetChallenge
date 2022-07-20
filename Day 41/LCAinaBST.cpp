6e91b6b0b843ec271bc03f264769d0ae42ff3aee
Mohammad Farukh <65119875+iamFarukh@users.noreply.github.com>
2022-07-20 12:18:32 +0530

    Create LCAinaBST.cpp
    LCAinaBST

---
+++ Day 41/LCAinaBST.cpp
@@ -0,0 +1,43 @@
+/************************************************************
+    Following is the Binary Search Tree node structure
+    
+    template <typename T>
+    class TreeNode {
+        public :
+        T data;
+        TreeNode<T> *left;
+        TreeNode<T> *right;
+
+        TreeNode(T data) {
+            this -> data = data;
+            left = NULL;
+            right = NULL;
+        }
+
+        ~TreeNode() {
+            if(left)
+                delete left;
+            if(right)
+                delete right;
+        }
+    };
+
+************************************************************/
+
+TreeNode<int>* LCAinaBST(TreeNode<int>* root, TreeNode<int>* P, TreeNode<int>* Q)
+{
+   if(root == NULL){
+       return NULL;
+   }
+   while(root){
+       if(root -> data > P->data && root->data > Q->data){
+           root = root -> left;
+       }
+       else if(root -> data < P->data && root->data < Q->data){
+           root = root -> right;
+       }
+       else{
+           return root;
+       }
+   }
+}
